d86fa507835c90a556abe2bcc30b8169e3303411
Jianing Yang <jianingy.yang@gmail.com>
2011-11-24 15:45:48 +0800

    changed some color settings

--- init.el
+++ init.el
@@ -53,5 +53,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-hide ((t (:background "#2e3436" :foreground "#2e3436"))))
+ '(org-block-begin-line ((t (:background "transparent" ))))
+ '(org-block-end-line ((t (:background "transparent" ))))
+ '(org-hide ((t (:background "transparent" :foreground "#2e3436"))))
  '(tabbar-default ((t (:weight thin :height 60 :family "PF Tempesta Seven")))))
