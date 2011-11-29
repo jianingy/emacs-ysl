675be39a357539b1cde5e8892434831a8ed440e4
Jianing Yang <jianingy.yang@gmail.com>
2011-11-29 09:46:18 +0800

    applied respective themes for window emacs and emacs client

--- etc-lisp/ysl-color-theme.el
+++ etc-lisp/ysl-color-theme.el
@@ -3,16 +3,36 @@
 (require 'color-theme)
 (add-search-path "themes")
 
-(if window-system
-    (progn
-      (require 'color-theme-tango)
-      (color-theme-tango)
-      (custom-set-faces
-       '(org-hide (( t (:background "#2e3436" :foreground "#2e3436"))))
-       ))
+(defun ysl/enable-window-theme ()
+  (progn
 
+    (require 'color-theme-tango)
+    (color-theme-tango)
+
+    (custom-set-faces
+     '(org-hide (( t (:background "#2e3436" :foreground "#2e3436"))))
+     )))
+
+(defun ysl/enable-terminal-theme ()
   (progn
     (require 'color-theme-ir-black)
     (color-theme-ir-black)))
+
+;; INSIPRED BY http://emacs-fu.blogspot.com/2009/03/color-theming.html
+
+(defun ysl/select-color-theme (frame)
+; must be current local ctheme
+  (select-frame frame)
+  (if (window-system frame)
+      (ysl/enable-window-theme)
+    (ysl/enable-terminal-theme)))
+
+(add-hook 'after-make-frame-functions 'ysl/select-color-theme)
+
+(setq color-theme-is-global nil)
+(if (window-system)
+    (ysl/enable-window-theme)
+  (ysl/enable-terminal-theme))
+
 
 (provide 'ysl-color-theme)
