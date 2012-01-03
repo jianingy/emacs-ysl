bf3942beb76c6440b3e7766b02f4dc22fa204f21
Jianing Yang <jianingy.yang@gmail.com>
2012-01-03 11:25:00 +0800

    start rainbow-mode while in css-mode

--- etc-lisp/ysl-css.el
+++ etc-lisp/ysl-css.el
@@ -4,6 +4,7 @@
 (setq css-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
 (autoload 'css-mode "css-mode" nil t)
+(add-hook 'css-mode-hook 'rainbow-mode)
 ;; }}
 
 (provide 'ysl-css)
