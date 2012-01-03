b3edf1277201c44619dea2898389aeaa4f5dae5e
Jianing Yang <jianingy.yang@gmail.com>
2012-01-03 20:14:16 +0800

    updated default font setting to emacs24 way

--- etc-lisp/ysl-font.el
+++ etc-lisp/ysl-font.el
@@ -9,13 +9,14 @@
                                  :family "PF Tempesta Seven"))))))
 
 ;; font setup function {{
-(defun ysl/set-x-font () 
-  (let ((fontset "fontset-default"))
-    (set-default-font ysl/x-font-en) 
-    (dolist (charset '(kana han symbol cjk-misc bopomofo)) 
-      (set-fontset-font fontset charset ysl/x-font-zh))))
+(defun ysl/set-x-font ()
+  (set-face-attribute 'default nil :font ysl/x-font-en)
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset ysl/x-font-zh))
+)
 ;; }}
 
-(if window-system 
+(if window-system
     (ysl/set-x-font))
 (provide 'ysl-font)
