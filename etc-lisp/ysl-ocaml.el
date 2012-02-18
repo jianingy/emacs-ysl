2e1c23caade27b7cae0b2606e7dd74dd52659a79
Jianing Yang <jianingy.yang@gmail.com>
2012-02-18 08:10:15 +0800

    replace caml-mode with tuareg-mode

--- etc-lisp/ysl-ocaml.el
+++ etc-lisp/ysl-ocaml.el
@@ -1,12 +1,19 @@
 (require 'ysl-init)
 
-(setq auto-mode-alist
-          (cons '("\\.ml[iyl]?$" .  caml-mode) auto-mode-alist))
-
-(add-search-path "site-lisp/ocaml")
-
-(autoload 'caml-mode "ocaml" (interactive)
-  "Major mode for editing Caml code." t)
+;; {{ disable ocaml-mode since we start to use tugreg-mode
+;;(setq auto-mode-alist
+;;          (cons '("\\.ml[iyl]?$" .  caml-mode) auto-mode-alist))
+;;(add-search-path "site-lisp/ocaml")
+;; (autoload 'caml-mode "ocaml" (interactive)
+;; "Major mode for editing Caml code." t)
 ;; (autoload 'camldebug "camldebug" (interactive) "Debug caml mode")
+;; }}
 
+;; {{ tuareg-mode
+(add-search-path "site-lisp/tuareg-2.0.4")
+(add-search-path "site-lisp/tuareg-caml-mode")
+(add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . tuareg-mode))
+(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+;; }}
 (provide 'ysl-ocaml)
--- etc-lisp/ysl-package.el
+++ etc-lisp/ysl-package.el
@@ -56,6 +56,7 @@
                :type git
                :url "http://github.com/yoshiki/yaml-mode.git"
                :features yaml-mode)
+
         (:name yasnippet-bundle :type elpa)))
 
 (setq ysl-packages
