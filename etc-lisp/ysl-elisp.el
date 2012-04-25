1a32dc21a9ba5be8ae28d7fa1c7e4043973fd451
Jianing Yang <jianingy.yang@gmail.com>
2012-04-26 00:39:41 +0800

    enable slime-mode

--- etc-lisp/ysl-elisp.el
+++ etc-lisp/ysl-elisp.el
@@ -1,18 +1,17 @@
 (require 'ysl-init)
 
 (add-hook 'emacs-lisp-mode-hook
-          (lambda () 
-                       (local-set-key "\C-c\C-c" 'eval-buffer)
-					   (setq indent-tabs-mode nil)))
-
+          (lambda ()
+            (local-set-key "\C-c\C-c" 'eval-buffer)
+            (setq indent-tabs-mode nil)))
 
 ;; slime for common-lisp editing {{
-;(require 'slime-autoloads)
-;(slime-setup '(slime-fancy))
+(require 'slime-autoloads)
+(slime-setup '(slime-fancy))
 
 (add-hook 'slime-mode-hook
-          (lambda () (progn
-                       (local-set-key "\C-c\C-q" 'slime-close-all-parens-in-sexp))))
+          (lambda ()
+            (local-set-key "\C-c\C-q" 'slime-close-all-parens-in-sexp)))
 ;; }}
 
 (provide 'ysl-elisp)
