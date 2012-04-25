807ee9bcd2b64352d2bd575b2b51603eed33895a
Jianing Yang <jianingy.yang@gmail.com>
2012-04-25 23:09:36 +0800

    enabled rainbow-mode css

--- etc-lisp/ysl-css.el
+++ etc-lisp/ysl-css.el
@@ -1,6 +1,7 @@
 (require 'ysl-init)
 
 ;; CSS Mode {{
+(require 'rainbow-mode)
 (setq css-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
 (autoload 'css-mode "css-mode" nil t)
