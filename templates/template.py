ff425a69960a12211c0e6b5e539484d7584946fb
Jianing YANG <jianingy@unitedstack.com>
2013-07-02 23:06:00 +0800

    update python template

--- templates/template.py
+++ templates/template.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# filename   : `(buffer-name)`
-# created at : `(format-time-string "%F %T")`
-# author     : `user-full-name` <`user-mail-address`>
-
-__author__ = '`user-full-name` <`user-mail-address`>'
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
