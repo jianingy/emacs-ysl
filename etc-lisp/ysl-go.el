1af48ac6e24dcc53adad7623beda0cf27ef7239b
Jianing Yang <jianingy@unitedstack.com>
2013-12-26 19:02:16 +0800

    clean up ysl-go.el

--- etc-lisp/ysl-go.el
+++ etc-lisp/ysl-go.el
@@ -1,18 +1,11 @@
-;; filename   : ysl-go.el
-;; created at : 2013-09-02 15:40:08
-;; author     : Jianing Yang <jianingy@unitedstack.com>
+;;; ysl-go.el --- go configuration
+;;; created at : 2013-09-02 15:40:08
+;;; author     : Jianing Yang <jianingy@unitedstack.com>
+;;; Commentary:
 
+;;; Code:
 (defvar ysl/go-executable "/usr/local/go/bin/go")
 (defvar ysl/gofmt-executable "/usr/local/go/bin/gofmt")
-
-(eval-after-load "go-mode"
-  '(progn
-     (flycheck-declare-checker go-fmt
-       "A Go syntax and style checker using the gofmt utility."
-       :command `(,ysl/gofmt-executable source-inplace)
-       :error-patterns '(("^\\(?1:.*\\):\\(?2:[0-9]+\\):\\(?3:[0-9]+\\): \\(?4:.*\\)$" error))
-       :modes 'go-mode)
-     (add-to-list 'flycheck-checkers 'go-fmt)))
 
 ;;; {{{
 (defun ysl/go-mode-hook ()
@@ -25,7 +18,7 @@
 
 ;; {{{ compile perl code
 (defun eval-buffer-as-go ()
-  "run buffer content as python program"
+  "Run buffer content as python program."
   (interactive)
   (save-buffer)
   (compilation-start (concat ysl/go-executable " run " (buffer-file-name))))
@@ -35,3 +28,4 @@
 (require 'go-mode)
 
 (provide 'ysl-go)
+;;; ysl-go.el ends here
