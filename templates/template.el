7f2efdeebb7d2b277a5df55f5a9e5ec9445cc48a
Jianing Yang <jianingy@unitedstack.com>
2013-12-26 22:07:46 +0800

    update autoinsert tempalte

--- templates/template.el
+++ templates/template.el
@@ -1,4 +1,6 @@
-;; filename   : `(buffer-name)`
-;; created at : `(format-time-string "%F %T")`
-;; author     : `user-full-name` <`user-mail-address`>
+;;; `(buffer-name)` ---
+;;; created at : `(format-time-string "%F %T")`
+;;; author     : `user-full-name` <`user-mail-address`>
+;;; Commentary:
 
+;;; Code:
--- templates/template.py
+++ templates/template.py
@@ -1,3 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!python2.7
 # vim: tabstop=4 shiftwidth=4 softtabstop=4 et
+#
+# filename   : `(buffer-name)`
+# author     : `user-full-name` <`user-mail-address`>
+# created at : `(format-time-string "%F %T")`
+#
